f363f3a8fda86b7a1e2e78195145e670d71a211b
Matt Haines <mhaines@Matt-H-MacBook-Pro.local>
2022-06-20 13:26:46 -0500

    updated card

--- src/Components/WeekForecast.tsx
+++ src/Components/WeekForecast.tsx
@@ -5,7 +5,14 @@ import WeatherCard from "./WeatherCard";
 export default function WeekForecast() {
   return (
     <div className={styles.Homepage}>
-      <WeatherCard />
+      <WeatherCard
+        tempHigh={90}
+        tempLow={80}
+        city={"Chicago"}
+        icon={"temp"}
+        width={500}
+        height={600}
+      />
     </div>
   );
 }
